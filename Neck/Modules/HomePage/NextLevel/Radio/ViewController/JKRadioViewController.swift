db7aead7082ac492f18a80928af1ec9e83b48789
worldunionYellow <kashjack@163.com>
2020-11-23 19:45:07 +0800

    创建页面

---
+++ Neck/Modules/HomePage/NextLevel/Radio/ViewController/JKRadioViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JKRadioViewController.swift
+//  Neck
+//
+//  Created by worldunionYellow on 2020/11/23.
+//  Copyright © 2020 worldunionYellow. All rights reserved.
+//
+
+import UIKit
+
+class JKRadioViewController: JKViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
